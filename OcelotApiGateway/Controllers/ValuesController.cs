f3bc032e4e0909d8b82d9a77b4a12d0870f973d6
RachaneeSaengkrajai <rachanee.saeng@gmail.com>
2019-07-18 13:53:30 +0700

    Initial commit

---
+++ OcelotApiGateway/Controllers/ValuesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace OcelotApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+        //private readonly IOcelotLogger _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ValuesController(
+            ILogger<ValuesController> logger,
+        //IOcelotLoggerFactory loggerFactory, 
+        IHttpContextAccessor httpContextAccessor
+        )
+        {
+            _logger = logger;
+            //_logger = loggerFactory.CreateLogger<ValuesController>();
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //var x = Get("");
+            //_logger.LogInformation($"-*-*-*-*-*- {x}");
+            _logger.LogInformation("Index page says hello");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public string Get(string key)
+        {
+            return _httpContextAccessor.HttpContext.TraceIdentifier;
+            //object obj;
+
+            //if (_httpContextAccessor.HttpContext == null || _httpContextAccessor.HttpContext.Items == null)
+            //{
+            //    return $"Unable to find data for key: {key} because HttpContext or HttpContext.Items is null";
+            //}
+
+            //if (_httpContextAccessor.HttpContext.Items.TryGetValue(key, out obj))
+            //{
+            //    var data = (string)obj;
+            //    return data;
+            //}
+
+            //return $"Unable to find data for key: {key}";
+        }
+    }
+}
---
+++ OcelotApiGateway/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using NLog.Web;
+
+namespace OcelotApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webhost = CreateWebHostBuilder(args).Build();
+            webhost.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .CaptureStartupErrors(true) // the default
+                .UseSetting("detailedErrors", "true")
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddJsonFile("ocelot.json")
+                        .AddEnvironmentVariables();
+                })
+                .UseNLog()
+                .UseStartup<Startup>();
+        //.ConfigureKestrel((context, options) =>
+        //{
+        //    options.ConfigureEndpoints();
+        //});
+    }
+}
